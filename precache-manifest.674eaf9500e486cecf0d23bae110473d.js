9aad2e8036ed7f79c3ba2ebda6917d7c5a507cdd
celes94 <celesclaks@yahoo.com>
2020-06-15 02:44:34 +0300

    Updates

---
+++ precache-manifest.674eaf9500e486cecf0d23bae110473d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a66b04c020d029460137e8e15c07551",
+    "url": "/index.html"
+  },
+  {
+    "revision": "babf387fede7ec36ce6a",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "babf387fede7ec36ce6a",
+    "url": "/static/js/2.6c6ac990.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6c6ac990.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bed288978fc4d785ae3",
+    "url": "/static/js/main.f3f2bd80.chunk.js"
+  },
+  {
+    "revision": "f866cd04515fe9e59514",
+    "url": "/static/js/runtime-main.8bcc4eee.js"
+  }
+]);
